c123d2ecd0d1642782feaa45b1d42a17146e500f
Yukun Guo <gyk.net@gmail.com>
2015-12-24 23:58:50 +0800

    Optional dependency on jQuery

--- scripts/main.js
+++ scripts/main.js
@@ -5,12 +5,25 @@ if (typeof requirejs === typeof undefined) {
 
 requirejs.config({
     paths: {
+        'jquery': 'jquery', 
         'underscore': 'underscore'
     }
 });
 
 requirejs(['underscore', 'mineSweeper'], function (_, MineSweeper) {
-    var ms = new MineSweeper({level: "Trivial"});
-    ms.print();
-    ms.interact();
+    // Optional dependency on jQuery
+    requirejs(['jquery'], function ($) {
+        $('#start-game').click(function (ev) {
+            $('#start-game').after('<div id="game"></div>');
+        });
+    }, function (err) {
+        if (typeof window !== typeof undefined) {
+            throw new Error("Failed to load jQuery");
+        }
+
+        requirejs.undef('jquery');
+        var ms = new MineSweeper({level: "Trivial"});
+        ms.print();
+        ms.interact();
+    });
 });

a51eddfc9130ebf56ddd6c94559029eed80d2006
Yukun Guo <gyk.net@gmail.com>
2016-01-25 22:51:29 +0800

    r.js optimization

---
+++ build.js
@@ -0,0 +1,13 @@
+// builds into a single file
+{
+    baseUrl: './scripts',
+    include: ['main', 'mineSweeper', 'mineSweeperView', 'timer', 'utility', 
+        'jquery', 'underscore'],
+    insertRequire: ['main'],  // almond.js doesn't look for data-main
+    name: 'almond',
+    paths: {
+        'jquery': 'jquery'  // uses the local version
+    },
+    out: 'scripts/require-built.js',  // should be renamed to require.js later
+    optimizeCss: 'standard'
+}
